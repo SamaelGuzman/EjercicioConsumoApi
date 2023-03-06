a77f25c64e8479a46fd95b339478115b506b0111
SamaelGuzman <samaelguzman@icloud.com>
2023-03-06 02:37:58 -0600

    Se creo una tabla dinamica que trae informacion desde una api publica

---
+++ js/index.js
@@ -0,0 +1,28 @@
+ 
+const listUsers = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  let tableBody = ``;
+  users.forEach((user) => {
+    tableBody += `<tr>
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+        <td>${user.website}</td>
+        
+        
+        
+        
+        
+        </tr>`;
+
+    // document.getElementById("tableBody_Users").innerHTML = tableBody
+    tableBody_Users.innerHTML = tableBody;
+  });
+};
+
+window.addEventListener("load", function () {
+  listUsers();
+});
