b23048731df7fdba7732fb09f9270760c377de40
SamaelGuzman <samaelguzman@icloud.com>
2023-03-06 14:43:46 +0100

    Se creo la seccion posts, se hizo la llamada a la API de posts y se hizo la interfaz para mostarlos

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
+ // Tabla de usuarios
+
  
 const listUsers = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await response.json();
 
-  let tableBody = ``;
+  let tableBodyUsr = ``;
   users.forEach((user) => {
-    tableBody += `<tr>
+    tableBodyUsr += `<tr>
         <td>${user.id}</td>
         <td>${user.name}</td>
         <td>${user.username}</td>
@@ -19,12 +21,50 @@ const listUsers = async () => {
         
         </tr>`;
 
-    // document.getElementById("tableBody_Users").innerHTML = tableBody
-    console.log(user)
-    tableBody_Users.innerHTML = tableBody;
+   document.getElementById("tableBody_Users").innerHTML = tableBodyUsr
+   
   });
 };
 
+
+//Tabla de Posts
+
+const listPosts = async () => {
+const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+const posts = await response.json()
+
+
+let tableBody_Posts = ``
+posts.forEach((post) => {
+tableBody_Posts+= `<tr>
+        <td>${post.id}</td>
+        <td>${post.title}</td>
+        <td>${post.body}</td>
+       
+        
+        
+        
+        
+        </tr>`
+document.getElementById("tableBody_Posts").innerHTML = tableBody_Posts
+
+
+});
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
 window.addEventListener("load", function () {
   listUsers();
+  listPosts();
 });
